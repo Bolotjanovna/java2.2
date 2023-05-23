0645d7a4d05787f6ac95305db462f3de061bfea3
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-05-23 23:09:19 +0500

    java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int score = 300;
+        int summa = 1100;
+        int reward = 11;
+
+        System.out.println((balance + score) + "бонусы");
+        System.out.println((balance + summa + reward) + "бонусы");
+    }
+}
